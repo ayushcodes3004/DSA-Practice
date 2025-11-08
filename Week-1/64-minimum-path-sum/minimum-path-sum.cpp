921a79ec80ecc48b21045730221f56a70983a89e
Ayush Jaiswal <ayushcodes3004@gmail.com>
2025-11-08 11:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (13.95%) - LeetSync

--- Week-1/64-minimum-path-sum/minimum-path-sum.cpp
+++ Week-1/64-minimum-path-sum/minimum-path-sum.cpp
@@ -21,7 +21,15 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return rec(0,0,m,n,grid,dp);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,INT_MAX));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1 && j==1)    dp[i][j]=grid[i-1][j-1];
+                else{
+                    dp[i][j]=grid[i-1][j-1]+min(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
