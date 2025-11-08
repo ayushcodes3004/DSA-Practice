2f41e72bcc7d851bf4abedb6c35f18176357d498
Ayush Jaiswal <ayushcodes3004@gmail.com>
2025-11-08 14:15:39 +0530

    Time: 178 ms (19.32%) | Memory: 17.2 MB (72.06%) - LeetSync

--- Week-1/416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ Week-1/416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 private:
     bool rec(int i,int j,int n,vector<int>& nums,vector<vector<int>>& dp){
         if(j==0)    return true;
-        if(j<0 || i==n) return false;
+        if(j<0 || i>=n) return false;
         if(dp[i][j]!=-1)    return dp[i][j];
         int take= rec(i+1,j-nums[i],n,nums,dp);
         int leave=rec(i+1,j,n,nums,dp);
@@ -15,7 +15,18 @@ public:
         for(int num: nums)  sum+=num;
         if(sum%2)   return false;
         int target=sum/2;
-        vector<vector<int>>dp(n,vector<int>(1+target,-1));
-        return rec(0,target,n,nums,dp);
+        vector<vector<bool>>dp(n+1,vector<bool>(1+target,false));
+        for(int i=0;i<=n;i++)   dp[i][0]=true;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                int leave=dp[i-1][j];
+                int take=0;
+                if(j>=nums[i-1]){
+                    take=dp[i-1][j-nums[i-1]];
+                }
+                dp[i][j]=(take||leave);
+            }
+        }
+        return dp[n][target];
     }
 };
