82eba2bf22a4144e0c327288e679fbfd59b890a3
Ayush Jaiswal <ayushcodes3004@gmail.com>
2025-11-12 00:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (90.23%) - LeetSync

--- Week-2/801-is-graph-bipartite/is-graph-bipartite.cpp
+++ Week-2/801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,34 +1,25 @@
 class Solution {
 private:
-    bool dfs(int node, int col, vector<int> &color,vector<vector<int>>& graph ){
-        color[node]=col;
+    // bool dfs(int node, int col, vector<int> &color,vector<vector<int>>& graph ){
+    //     color[node]=col;
 
-        for(auto it: graph[node]){
-            if(color[it]==-1){
-                if(!dfs(it, 1 - col, color, graph))
-                    return false;
-            }
-            else if(color[it]==col){
-                return false;
-            }
-        }
-        return true;
-    }
-    bool bfs(int src,int c,vector<int>& color, vector<vector<int>>& adj){
-        queue<int>q;
-        q.push(src);
-        color[src]=c;
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            for(auto neigh: adj[node]){
-                if(color[neigh]==-1){
-                    color[neigh]=!color[node];
-                    q.push(neigh);
-                }else if(color[neigh]==color[node]){
-                    return false;
-                }
-            }
+    //     for(auto it: graph[node]){
+    //         if(color[it]==-1){
+    //             if(!dfs(it, 1 - col, color, graph))
+    //                 return false;
+    //         }
+    //         else if(color[it]==col){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    bool dfs(int node,int c,vector<int>& color, vector<vector<int>>& adj){
+        color[node]=c;
+        for(int neigh: adj[node]){
+            if(color[neigh]==-1){
+                if(!dfs(neigh,1-c,color,adj))   return false;
+            }else if(color[neigh]==color[node]) return false;
         }
         return true;
     }
@@ -40,7 +31,7 @@ public:
 
         for(int i=0;i<V;i++){
             if(color[i]==-1){
-                if(!bfs(i, 0, color, graph))
+                if(!dfs(i, 0, color, graph))
                     return false;
             }
         }
